9674f35c39d2625563cf56653628164d2cc877c6
Alejandro Avilés <ome@unconventional.dev>
2023-07-14 04:11:11 +0200

    Add @patch_enum decorator

---
+++ src/indico_patcher/enum.py
@@ -0,0 +1,71 @@
+# This file is part of indico-patcher.
+# Copyright (C) 2023 UNCONVENTIONAL
+
+from enum import Enum
+
+from aenum import extend_enum
+
+from indico.util.enum import RichIntEnum
+
+# Attributes used to store data for rich properties in RichEnum
+RICH_ENUM_BASE_ATTRS = ("__titles__", "__css_classess__")
+
+
+# TODO: Modify RichIntEnum upstream so that it's possible to programmatically
+#       determine which attributes are used for rich properties. This will
+#       avoid having to explicitly specify additional ones in the decorator.
+def patch_enum(enum: Enum, *, padding: int = 0, extra_attrs: tuple = (), rich_attrs: tuple = ()):
+    """Patch an Enum with members and attributes from a decorated one.
+
+    :param padding: Value padding for patched enum members. Useful to avoid
+                    collisions with future members in the original enum.
+    :param extra_args: Additional attributes that should be carried over to the
+                       original enum.
+    :param rich_attrs: Additional attributes used for per-member rich information
+                       in subclasses of RichIntEnum.
+    """
+    if not issubclass(enum, Enum):
+        raise TypeError("The 'enum' argument must be a subclass of Enum.")
+    if padding < 0:
+        raise ValueError("Padding value cannot be negative.")
+
+    # Determine which rich attributes need to be patched
+    _rich_attrs = set()
+    if rich_attrs and not issubclass(enum, RichIntEnum):
+        raise ValueError("The argument 'rich_attrs' can only be used for subclasses of RichIntEnum.")
+    if issubclass(enum, RichIntEnum):
+        _rich_attrs.update(RICH_ENUM_BASE_ATTRS + rich_attrs)
+
+    # Make sure that extra attributes don't override rich attributes in the original enum
+    if collision := _rich_attrs.intersection(set(extra_attrs)):
+        raise ValueError(f"The original Emum already defines a '{list(collision)[0]}' attribute for rich information.")
+
+    def wrapper(patch):
+        if not issubclass(patch, Enum):
+            raise TypeError("The patch must be a subclass of Enum.")
+        # Extend original enum with members from patch
+        for x in patch:
+            extend_enum(enum, x.name, x.value + padding)
+        # Extend original enum with per-member rich values
+        for attr in _rich_attrs:
+            _patch_rich_attr(patch, attr)
+        # Set extra attributes in the original enum
+        for attr in extra_attrs:
+            value = getattr(patch, attr)
+            setattr(enum, attr, value)
+
+    def _patch_rich_attr(patch, attr):
+        """Patch the rich attribute af a RichIntEnum."""
+        if not all(hasattr(x, attr) for x in (enum, patch)):
+            return
+        orig_rich_values = getattr(enum, attr)
+        patch_rich_values = getattr(patch, attr)
+        # Determine padding for list of rich values
+        padding_values = []
+        if padding:
+            idx = padding - len(orig_rich_values)
+            padding_values = [None] * idx
+        # Set the new list of rich values in the original enum
+        setattr(enum, attr, orig_rich_values + padding_values + patch_rich_values)
+
+    return wrapper
---
+++ tests/test_enum.py
@@ -0,0 +1,143 @@
+# This file is part of indico-patcher.
+# Copyright (C) 2023 UNCONVENTIONAL
+
+from enum import Enum
+
+import pytest
+from pytest import raises
+
+from indico.util.enum import RichIntEnum
+
+from indico_patcher.enum import patch_enum
+
+
+class Arcana(Enum):
+    major = 1
+    minor = 2
+
+
+@pytest.fixture
+def TarotCard():
+    class TarotCard(Enum):
+        the_fool = 0
+        the_magician = 1
+        the_high_priestess = 2
+
+    return TarotCard
+
+
+@pytest.fixture
+def RichTarotCard():
+    class RichTarotCard(RichIntEnum):
+        __deck__ = "Rider-Waite Tarot"
+        __titles__ = ["The Fool", "The Magician", "The High Priestess"]
+        __arcana__ = [Arcana.major, Arcana.major, Arcana.major]
+        the_fool = 0
+        the_magician = 1
+        the_high_priestess = 2
+
+        @property
+        def arcana(self):
+            return self.__arcana__[self]
+
+    return RichTarotCard
+
+
+def test_patch_enum(TarotCard):
+    @patch_enum(TarotCard)
+    class _TarotCard(Enum):
+        the_empress = 3
+        the_emperor = 4
+
+    assert TarotCard.the_magician.value == 1
+    assert TarotCard.the_high_priestess.value == 2
+    assert TarotCard.the_empress.value == 3
+    assert TarotCard.the_emperor.value == 4
+
+
+def test_patch_enum_with_padding(TarotCard):
+    @patch_enum(TarotCard, padding=22)
+    class _TarotCardB(Enum):
+        the_one_of_wands = 1
+        the_two_of_wands = 2
+
+    assert TarotCard.the_one_of_wands.value == 23
+    assert TarotCard.the_two_of_wands.value == 24
+
+
+def test_patch_enum_with_negative_padding(TarotCard):
+    with raises(ValueError):
+        @patch_enum(TarotCard, padding=-1)
+        class _TarotCardA(Enum):
+            pass
+
+
+def test_patch_enum_only_for_enums(TarotCard):
+    with raises(TypeError):
+        @patch_enum(TarotCard)
+        class _TarotCard:
+            pass
+
+    with raises(TypeError):
+        @patch_enum(type)
+        class Foo(Enum):
+            pass
+
+
+def test_patch_enum_with_rich_attrs(TarotCard):
+    with raises(ValueError):
+        @patch_enum(TarotCard, rich_attrs=("__arcana__",))
+        class _TarotCard(Enum):
+            pass
+
+
+def test_patch_richenum(RichTarotCard):
+    @patch_enum(RichTarotCard, rich_attrs=("__arcana__",))
+    class _TarotCard(Enum):
+        __titles__ = ["The Empress", "The Emperor"]
+        __arcana__ = [Arcana.major, Arcana.major]
+        the_empress = 3
+        the_emperor = 4
+
+    assert RichTarotCard.the_fool.title == "The Fool"
+    assert RichTarotCard.the_magician.title == "The Magician"
+    assert RichTarotCard.the_high_priestess.title == "The High Priestess"
+    assert RichTarotCard.the_empress.title == "The Empress"
+    assert RichTarotCard.the_empress.arcana == Arcana.major
+    assert RichTarotCard.the_emperor.title == "The Emperor"
+    assert RichTarotCard.the_emperor.arcana == Arcana.major
+
+
+def test_patch_richenum_with_padding(RichTarotCard):
+    @patch_enum(RichTarotCard, padding=22, rich_attrs=("__arcana__",))
+    class _TarotCard(Enum):
+        __titles__ = [None, "The One of Wands", "The Two of Wands"]
+        __arcana__ = [None, Arcana.minor, Arcana.minor]
+        the_one_of_wands = 1
+        the_two_of_wands = 2
+
+    assert RichTarotCard.the_one_of_wands.title == "The One of Wands"
+    assert RichTarotCard.the_one_of_wands.arcana == Arcana.minor
+    assert RichTarotCard.the_two_of_wands.title == "The Two of Wands"
+    assert RichTarotCard.the_two_of_wands.arcana == Arcana.minor
+
+
+def test_patch_enum_with_extra_attrs(RichTarotCard):
+    @patch_enum(RichTarotCard, extra_attrs=("__deck__", "__meanings__",))
+    class _TarotCard(Enum):
+        __deck__ = "Hermetic Tarot"
+        __meanings__ = {
+            "the_fool": "New beginnings. Innocence. Leap of faith.",
+            "the_magician": "Mastery of skills. Power of manifestation. Elemental forces.",
+            "the_high_priestess": "Intuition. Hidden knowledge. Spiritual insight.",
+        }
+
+    assert RichTarotCard.__deck__ == "Hermetic Tarot"
+    assert hasattr(RichTarotCard, "__meanings__")
+
+
+def test_patch_enum_with_extra_attrs_collision(RichTarotCard):
+    with raises(ValueError):
+        @patch_enum(RichTarotCard, extra_attrs=("__titles__",))
+        class _TarotCard(Enum):
+            pass

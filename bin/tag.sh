d88ea97ea807a2951942c4a6ad3dd08912140a16
Alejandro Avilés <ome@unconventional.dev>
2024-04-03 21:55:42 +0200

    Add version tagging script

---
+++ bin/tag.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# This file is part of indico-patcher.
+# Copyright (C) 2023 - 2024 UNCONVENTIONAL
+
+set -e
+
+VERSION=$(grep 'version =' pyproject.toml | cut -d '"' -f 2)
+TAG="v${VERSION}"
+
+git tag "${TAG}" -m "Release ${TAG}."
+echo "Tag ${TAG} was created"

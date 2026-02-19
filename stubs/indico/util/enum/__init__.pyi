4b494ca3469f6ef96e661e4f6bf0047867e9c344
Alejandro Avilés <ome@unconventional.dev>
2026-02-19 14:32:33 +0100

    Silence mypy error in enum stubs

--- stubs/indico/util/enum/__init__.pyi
+++ stubs/indico/util/enum/__init__.pyi
@@ -1,6 +1,11 @@
 # This file is part of indico-patcher.
 # Copyright (C) 2023 - 2026 UNCONVENTIONAL
 
+# mypy: disable-error-code=misc
+# XXX: mypy warns on zero-member enums. These base enums are intentionally
+#      empty as they are simply used as stubs.
+#      See https://typing.readthedocs.io/en/latest/spec/enums.html#defining-members
+
 from enum import Enum
 from typing import Any
 
